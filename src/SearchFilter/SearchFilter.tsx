e14398611e30cb15364bb9a1a76f62f6f08ea207
Кирилл Попов <diplomatgmg@gmail.com>
2024-02-25 17:39:42 +0300

    Изменено имя папки

---
+++ src/SearchFilter/SearchFilter.tsx
@@ -0,0 +1,11 @@
+import React, { type ReactElement } from 'react'
+
+const SearchFilter = (): ReactElement => {
+  return (
+  <div>
+   SearchFilter component
+  </div>
+  )
+}
+
+export default SearchFilter
