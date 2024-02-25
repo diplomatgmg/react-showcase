cf5df4cf84c1177b19b7a7f2e91a67014ec07388
Кирилл Попов <diplomatgmg@gmail.com>
2024-02-25 19:13:27 +0300

    Улучшена отзывчивость компонента

--- src/SearchFilter/SearchFilter.tsx
+++ src/SearchFilter/SearchFilter.tsx
@@ -1,18 +1,53 @@
-import React, { type ChangeEvent, type ReactElement, useState } from 'react'
+import React, { type ChangeEvent, type ReactElement, useRef, useState } from 'react'
 import countries from './countries'
 import Card from '../components/Card'
 import './style.css'
 
 const SearchFilter = (): ReactElement => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
   const [input, setInput] = useState<string>('')
+  const [countrySelected, setCountrySelected] = useState<boolean>(false)
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>): void => {
     setInput(e.target.value)
+    setCountrySelected(false)
+  }
+
+  const handleClearInput = (): void => {
+    inputRef?.current?.focus()
+    setInput('')
+    setCountrySelected(false)
+  }
+
+  const handleSelectCountry = (country: string): void => {
+    setInput(country)
+    setCountrySelected(true)
   }
 
   const filteredCountries = countries.filter(country => {
+    if (countrySelected) {
+      return false
+    }
+
     return country.toLowerCase().includes(input.toLowerCase())
   })
+
+  const renderCountries = (): ReactElement | null => {
+    if (filteredCountries.length === 0) {
+      return <div>Страна не найдена...</div>
+    }
+    return (
+      <ul id="resultList" className="list-group mt-3 scrollable-list">
+        {filteredCountries.map(country => (
+          <li onClick={() => handleSelectCountry(country)}
+              key={country}
+              className="list-group-item list-group-item-action">
+            {country}
+          </li>
+        ))}
+      </ul>
+    )
+  }
 
   return (
     <Card name={'SearchFilter'} widthClass={'col-md-3'}>
@@ -20,15 +55,15 @@ const SearchFilter = (): ReactElement => {
         <input type="text"
                value={input}
                onChange={handleInput}
+               ref={inputRef}
                placeholder="Filter..."
                className="form-control"
                aria-label="Sizing example input"
                aria-describedby="inputGroup-sizing-default"/>
-        <button onClick={() => setInput('')} className="btn btn-outline-secondary" type="button">x</button>
+        <button onClick={handleClearInput} className="btn btn-primary" type="button">X</button>
       </div>
-      <ul id="resultList" className="list-group mt-3 scrollable-list">
-        {filteredCountries.map(country => <li onClick={() => setInput(country)} key={country} className="list-group-item list-group-item-action">{country}</li>)}
-      </ul>
+
+      {renderCountries()}
     </Card>
   )
 }
