dd61dfa07c441f8ffa60630d70ab60a402d0852d
Кирилл Попов <diplomatgmg@gmail.com>
2024-02-25 18:02:23 +0300

    Добавлена возможность менять ширину компонента

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,17 @@
 import React, { type ReactElement, type ReactNode } from 'react'
+import clsx from 'clsx'
 
 interface CardProps {
   name: string
+  widthClass?: string
   children?: ReactNode
 }
 
-const Card = ({ name, children }: CardProps): ReactElement => {
+const Card = ({ name, widthClass = '', children }: CardProps): ReactElement => {
+  const cardClass = clsx('card', 'p-5', 'rounded-4', widthClass)
+
   return (
-    <div className="card p-5 rounded-4">
+    <div className={cardClass}>
       <h5 className="card-title text-center mb-3 h2">{name}</h5>
       {children}
     </div>
